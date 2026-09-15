be4f25960d4eb9de3da3544ddad1d91ddbdf4d68
avramkinostap <av.ostap13@gmail.com>
2026-09-15 17:02:44 +0300

    Task 4: Weather Information App

--- task4_weather_app/weather_app.py
+++ task4_weather_app/weather_app.py
@@ -2,3 +2,14 @@ import requests
 
 city = input("Write city name: ")
 url = f"https://wttr.in/{city}?format=j1"
+
+try:
+    response = requests.get(url, verify=False)
+    data = response.json()
+
+    temperature = data["current_condition"][0]["temp_C"]
+    description = data["current_condition"][0]["weatherDesc"][0]["value"]
+
+    print(f"Weather in {city}: {temperature}°C, {description}")
+except Exception:
+    print("Could not get weather data. Please check the city name or your internet connection.")
